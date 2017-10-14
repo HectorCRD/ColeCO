1f59eecd74b892461e33072814c79db400c2fc52
Carlos Brys <carlos.brys@gmail.com>
2017-10-14 18:02:20 -0300

    Actualizo desde Dropbox

--- theme/js/report-form.js
+++ theme/js/report-form.js
@@ -1,18 +1,33 @@
 $(document).ready(function (){
 
+  var url_params = get_params();
+
+  if (typeof url_params['message'] == 'string') {
+    modal.open({content: '<img src="/theme/img/loading.gif" alt="Cargando"/>'});
+    $('#modal-content').empty();
+    $('#modal-content').html('<div class="message">' + url_params['message'] + '</div>');
+  }
+
   // Search callback
   $('#report').bind('click',function(e){
 
     // Avoiding reload of page after form submit
     e.preventDefault();
 
+    if(!$("#searchlink").hasClass("open")){
+       $("#searchlink").addClass("open");
+       $('#searchlink .fa-search').addClass('fa-times');
+       $('#searchlink .fa-search').removeClass('fa-search');
+       $('#report').hide();
+    }
+
     // Define modal
-    modal.open({content: '<img src="theme/img/loading.gif" alt="Cargando"/>'});
+    modal.open({content: '<img src="/theme/img/loading.gif" alt="Cargando"/>'});
     $('#modal-content').html(
       $('#modal-content').load('contact.html')
     )
 
-    $.get('contact.html', function(result) {
+    $.get('/contact.html', function(result) {
         $('#modal-content').val(result);
         // Obtain parameters from url
         var url_params = get_params();
